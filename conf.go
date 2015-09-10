3c6f64fbd88d0f060cc4faa61977af659f69a2d9
Caleb Spare <cespare@gmail.com>
2015-09-10 14:08:03 -0700

    Gofmt

--- conf.go
+++ conf.go
@@ -67,7 +67,7 @@ func filterNamespace(ns string) (string, error) {
 		return "", err
 	}
 	ns = strings.NewReplacer("%H", hostname).Replace(ns)
-	sanitized, ok, _, rest := parseKey([]byte(ns + ":"), false)
+	sanitized, ok, _, rest := parseKey([]byte(ns+":"), false)
 	if !ok || len(rest) > 0 {
 		return "", fmt.Errorf("Bad tag: %s", ns)
 	}
